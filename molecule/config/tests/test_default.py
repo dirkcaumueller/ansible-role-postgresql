2f2bb08c6f270675c3f3e438528d65bf90338b95
till <till@php.net>
2020-07-14 20:15:21 +0200

    Feature: enable configuration
     - introduce pre configure for values that need to be set first
     - restart the service when settings require it
     - add another molecule test to confirm behavior

---
+++ molecule/config/tests/test_default.py
@@ -0,0 +1,21 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_postgresql_is_running(host):
+    svc = host.service("postgresql-12")
+    assert svc.is_running
+    assert svc.is_enabled
